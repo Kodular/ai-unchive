d0040baadf64c84728a5206a9c1c14e384e3cab1
Vishwas-Adiga <vishwasadiga@gmail.com>
2019-11-11 22:27:42 +0530

    aia_reader: Add support for url reading

--- unchive/aia_reader.js
+++ unchive/aia_reader.js
@@ -1,6 +1,6 @@
 export class AIAReader {
   static read(content) {
-    var readerObj = content instanceof Blob ? new zip.BlobReader(content) : new zip.HttpRangeReader(content);
+    var readerObj = content instanceof Blob ? new zip.BlobReader(content) : new zip.HttpReader(content);
     zip.createReader(readerObj, (reader) => {
       reader.getEntries((entries) => {
         if (entries.length) {
