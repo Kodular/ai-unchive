7ae0895e4e64ebd9095d09e02eefa7b96beedaae
Vishwas-Adiga <vishwasadiga@gmail.com>
2019-11-11 22:16:04 +0530

    aia_reader.js: Update reader to accept URLs

--- unchive/aia_reader.js
+++ unchive/aia_reader.js
@@ -1,21 +1,13 @@
 export class AIAReader {
-  static read(blob) {
-    // use a BlobReader to read the zip from a Blob object
-    zip.createReader(new zip.BlobReader(blob), function(reader) {
-
-      // get all entries from the zip
-      reader.getEntries(function(entries) {
+  static read(content) {
+    zip.createReader(content instanceof Blob ? new zip.BlobReader(content) : new zip.HttpReader(content), (reader) => {
+      reader.getEntries((entries) => {
         if (entries.length) {
-          // get first entry content as text
           entries[0].getData(new zip.TextWriter(), function(text) {
-            // text contains the entry data as a String
             console.log(text);
-
-            // close the zip reader
             reader.close(function() {
               // onclose callback
             });
-
           }, function(current, total) {
             // onprogress callback
           });
