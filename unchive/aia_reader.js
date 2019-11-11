4b508c44ff69c088039b81434f8e89d7ac021fdb
Vishwas-Adiga <vishwasadiga@gmail.com>
2019-11-11 22:48:03 +0530

    aia_reader.js: Read all files in aia

--- unchive/aia_reader.js
+++ unchive/aia_reader.js
@@ -1,17 +1,19 @@
+import { Screen } from './screen.js'
+
 export class AIAReader {
   static read(content) {
     var readerObj = content instanceof Blob ? new zip.BlobReader(content) : new zip.HttpReader(content);
     zip.createReader(readerObj, (reader) => {
       reader.getEntries((entries) => {
         if (entries.length) {
-          entries[0].getData(new zip.TextWriter(), function(text) {
-            console.log(text);
-            reader.close(function() {
-              // onclose callback
-            });
-          }, function(current, total) {
-            // onprogress callback
-          });
+          for(var entry of entries) {
+            entry.getData(new zip.TextWriter(), function(text) {
+              console.log(text);
+              reader.close(function() {
+                // onclose callback
+              });
+            }
+          }
         }
       });
     }, function(error) {
