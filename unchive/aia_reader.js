efee7b9d5dd90202cf48062d09b49a164322148b
Vishwas-Adiga <vishwasadiga@gmail.com>
2019-11-11 22:22:13 +0530

    aia_reader.js: Fix constructor issue

--- unchive/aia_reader.js
+++ unchive/aia_reader.js
@@ -1,6 +1,7 @@
 export class AIAReader {
   static read(content) {
-    zip.createReader(content instanceof Blob ? new zip.BlobReader(content) : new zip.HttpReader(content), (reader) => {
+    var readerObj = content instanceof Blob ? new zip.BlobReader(content) : new zip.HttpReader(content);
+    zip.createReader(readerObj, (reader) => {
       reader.getEntries((entries) => {
         if (entries.length) {
           entries[0].getData(new zip.TextWriter(), function(text) {
