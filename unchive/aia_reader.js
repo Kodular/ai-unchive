aa1aa04b55e0ef1b110d421c49f9fcad029ee688
Vishwas-Adiga <vishwasadiga@gmail.com>
2019-11-12 00:45:06 +0530

    test

--- unchive/aia_reader.js
+++ unchive/aia_reader.js
@@ -2,26 +2,49 @@ import { Screen } from './screen.js'
 
 export class AIAReader {
   static read(content) {
-    var screens = [];
+    this.screens = [];
     var readerObj = content instanceof Blob ? new zip.BlobReader(content) : new zip.HttpReader(content);
     zip.createReader(readerObj, (reader) => {
       reader.getEntries((entries) => {
         if (entries.length) {
-          for(let entry of entries) {
-            entry.getData(new zip.TextWriter(), function(text) {
-              console.log(text);
-              if(entry.filename.split('.')[1] == 'bky') {
+          this.splitEntries(entries);
+          this.fetchBlockData();
+          this.fetchSchemeData();
 
-              }
-              reader.close(function() {
-                // onclose callback
-              });
-            });
-          }
+          console.log(JSON.stringify(this.screen));
         }
       });
     }, function(error) {
       // onerror callback
     });
+  }
+
+  splitEntries(entries) {
+    this.schemes = entries.filter((entry) => {
+      return entry.filename.split('.')[1] == 'scm';
+    });
+
+    this.blocks = entries.filter((entry) => {
+      return entry.filename.split('.')[1] == 'blk';
+    });
+  }
+
+  fetchBlockData() {
+    for(let blk of this.blocks) {
+      blk.getData(new zip.TextWriter(), function(text) {
+        this.screens.push({
+          'name' : blk.split('/').pop().split('.')[0],
+          'screen' : new Screen('', text);
+        })
+      });
+    }
+  }
+
+  fetchSchemeData() {
+    for(let scm of this.schemes) {
+      scm.getData(new zip.TextWriter(), function(text) {
+        this.screens.find(x => x.name == scm.filename.split('/').pop().split('.')[0]).screen.setScheme(text);
+      });
+    }
   }
 }
