cd39450bb9e6e76c25ad069d4e1ba55a28678c11
Vishwas-Adiga <vishwasadiga@gmail.com>
2019-11-11 16:16:34 +0530

    views: Add root panel to body

--- views/screen.js
+++ views/screen.js
@@ -1,11 +1,7 @@
 import { View } from './view.js'
 
 class Screen extends View {
-  constructor(name) {
+  constructor() {
     super('DIV');
-    this.name = name;
-    alert(this.name);
   }
 }
-
-let s = new Screen('aa');
