115757ec434249edba50dbd192e16e8cb3040b71
Vishwas-Adiga <vishwasadiga@gmail.com>
2019-11-12 23:51:36 +0530

    Update property_processor.js

--- unchive/property_processor.js
+++ unchive/property_processor.js
@@ -3,5 +3,18 @@ this.addEventListener('message', function(e) {
   var propertyJSON = e.data.propertyJson;
   var descriptorJSON = e.data.descriptorJSON;
 
-  this.postMessage({'properties' : []});
+  var properties = [];
+  for(let property of descriptorJSON.find(x => x.type == type).properties) {
+    if(propertyJSON.hasOwnProperty(property.name))
+      properties.push({
+        'name' : property.name,
+        'value' : propertyJSON[property.name]
+      });
+    else
+      properties.push({
+        'name' : property.name,
+        'value' : property.defaultValue
+      });
+  }
+  this.postMessage({'properties' : properties});
 }, false);
