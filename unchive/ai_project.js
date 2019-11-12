5b79e483d27137056f91ea745a36ff3f588c9618
Vishwas-Adiga <vishwasadiga@gmail.com>
2019-11-12 20:36:51 +0530

    update xml lib

--- unchive/ai_project.js
+++ unchive/ai_project.js
@@ -58,7 +58,7 @@ export class AIScreen {
 
   generateBlocks(blkXml) {
     // TODO: convert xml to json, and then to block objects
-    var blockJson = xml2json(blkXml);
+    var blockJson = xml2json(new DOMParser().parseFromString(blkXml, 'text/xml'));
     this.blocks = blockJson;
   }
 }
