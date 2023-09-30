16ff3d34ee58c37b96d062ca4e5bc19b06f2cdfe
Pavitra Golchha <pavitra@kodular.io>
2023-10-01 03:52:42 +0530

    Replace Recharts with Mantine components

--- src/components/OverviewTab.tsx
+++ src/components/OverviewTab.tsx
@@ -1,84 +1,99 @@
-import {Cell, LabelList, Legend, Pie, PieChart, ResponsiveContainer} from "recharts";
 import React from "react";
 import {AIProject} from "../unchive/ai_project";
-import {Avatar, Divider, Grid, Group, ScrollArea, Stack, Text, TextInput} from "@mantine/core";
+import {Avatar, Badge, Divider, Grid, Group, RingProgress, ScrollArea, Stack, Text, TextInput} from "@mantine/core";
 import {getPackageName} from "../utils";
 
 const COLORS = ['#3366cc', '#dc3912', '#ff9900', '#109618', '#5e35b1'];
 
-function RenderPieChart({data}: { data: { name: string, value: number }[] }) {
-    const sortedData = data.sort((a, b) => b.value - a.value)
-    return (
-        <ResponsiveContainer>
-            <PieChart>
-                <Pie data={sortedData} dataKey="value" cx="50%" cy="50%" outerRadius={100} innerRadius={50}>
-                    {sortedData.map((_, index) => (
-                        <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]}/>
-                    ))}
-                    <LabelList dataKey="value" position="inside" formatter={(f: number) => (f * 100).toFixed(1) + '%'}/>
-                </Pie>
-                <Legend layout="vertical" align="right" verticalAlign="top" iconType="circle"/>
-            </PieChart>
-        </ResponsiveContainer>
-    );
+function RenderPieChart({data, title}: { data: { name: string, value: number }[], title: string }) {
+  const sections = data.map((d, i) => ({
+    value: d.value * 100,
+    color: COLORS[i % COLORS.length],
+    tooltip: `${d.name}: ${(d.value * 100).toFixed(1)}%`,
+    children: 123
+  }));
+
+  return (
+    <Group>
+      <RingProgress
+        size={300}
+        thickness={40}
+        label={
+          <Text size="xs" ta="center">
+            {title}
+          </Text>
+        }
+        sections={sections}
+      />
+      <Stack>
+        {data.map((d, i) => (
+          <Group justify="apart" key={i}>
+            <Badge variant="dot" color={COLORS[i % COLORS.length]} style={{textTransform: 'none'}}>{d.name}</Badge>
+            <Text>{(d.value * 100).toFixed(1)}%</Text>
+          </Group>
+        ))}
+      </Stack>
+    </Group>
+  )
 }
 
 function ProjectPropertiesPanel({properties}: { properties: { name: string, value: string }[] }) {
-    return (
-        <div>
-            <Group justify="apart" style={{padding: '8px 4px'}}>
-                <div>Project Properties</div>
-            </Group>
-            <Divider/>
-            <ScrollArea offsetScrollbars scrollbarSize={6} scrollHideDelay={300}
-                        styles={{viewport:{height:"calc(100dvh - var(--app-shell-header-height) - 82px)"}}}>
-                <Stack gap='xs'>
-                    {
-                        properties.map((property, i) => (
-                            <TextInput key={i} label={property.name} value={property.value} readOnly/>
-                        ))
-                    }
-                </Stack>
-            </ScrollArea>
-        </div>
-    );
+  return (
+    <div>
+      <Group justify="apart" style={{padding: '8px 4px'}}>
+        <div>Project Properties</div>
+      </Group>
+      <Divider/>
+      <ScrollArea offsetScrollbars scrollbarSize={6} scrollHideDelay={300}
+                  styles={{viewport: {height: "calc(100dvh - var(--app-shell-header-height) - 82px)"}}}>
+        <Stack gap='xs'>
+          {
+            properties.map((property, i) => (
+              <TextInput key={i} label={property.name} value={property.value} readOnly/>
+            ))
+          }
+        </Stack>
+      </ScrollArea>
+    </div>
+  );
 }
 
 export function OverviewTab({project}: { project: AIProject }) {
-    const totalBlocks = project.screens.reduce((a, s) => a + (s.blocks?.match(/<\/block>/g) || []).length, 0)
-    const blocksPerScreen = project.screens.map(s => ({
-        name: s.name,
-        value: (s.blocks?.match(/<\/block>/g) || []).length / totalBlocks
-    }))
+  const totalBlocks = project.screens.reduce((a, s) => a + (s.blocks?.match(/<\/block>/g) || []).length, 0)
+  const blocksPerScreen = project.screens.map(s => ({
+    name: s.name,
+    value: (s.blocks?.match(/<\/block>/g) || []).length / totalBlocks
+  }))
 
-    const totalAssets = project.assets.length
-    const assetsPerType = Object.entries(project.assets.reduce((a: Record<string, number>, s) => {
-        if (a[s.type]) {
-            a[s.type]++
-        } else {
-            a[s.type] = 1
-        }
-        return a
-    }, {})).map(([k, v]) => ({
-        name: k,
-        value: v / totalAssets
-    }))
+  const totalAssets = project.assets.length
+  const assetsPerType = Object.entries(project.assets.reduce((a: Record<string, number>, s) => {
+    if (a[s.type]) {
+      a[s.type]++
+    } else {
+      a[s.type] = 1
+    }
+    return a
+  }, {})).map(([k, v]) => ({
+    name: k,
+    value: v / totalAssets
+  }))
 
-    return (
-        <Grid>
-            <Grid.Col span={3}>
-                <Avatar radius="md" size="xl" color="dark" src="/logo.png"/>
-                <Text>Package Name = {getPackageName(project)}</Text>
-            </Grid.Col>
-            <Grid.Col span={3}>
-                <ProjectPropertiesPanel properties={project.properties}/>
-            </Grid.Col>
-            <Grid.Col span={6}>
-                <ScrollArea offsetScrollbars>
-                    <RenderPieChart data={blocksPerScreen}/>
-                    <RenderPieChart data={assetsPerType}/>
-                </ScrollArea>
-            </Grid.Col>
-        </Grid>
-    )
+  return (
+    <Grid>
+      <Grid.Col span={3}>
+        <Avatar radius="md" size="xl" color="dark" src="/logo.png"/>
+        <Text>Package Name = {getPackageName(project)}</Text>
+      </Grid.Col>
+      <Grid.Col span={3}>
+        <ProjectPropertiesPanel properties={project.properties}/>
+      </Grid.Col>
+      <Grid.Col span={6}>
+        <ScrollArea offsetScrollbars scrollbarSize={6} scrollHideDelay={300}
+                    styles={{viewport: {height: "calc(100dvh - var(--app-shell-header-height) - 41px)"}}}>
+          <RenderPieChart data={blocksPerScreen} title="Blocks per screen"/>
+          <RenderPieChart data={assetsPerType} title="Assets per type"/>
+        </ScrollArea>
+      </Grid.Col>
+    </Grid>
+  )
 }
